4efe88459ca55f2f086f54ba465002a91903e880
DevolperRL <100154298+DevolperRL@users.noreply.github.com>
2024-09-15 17:41:31 +0200

    LunaInvisibleBox.lua

---
+++ Tooltip/LunaInvisibleBox.lua
@@ -0,0 +1,14 @@
+--[[
+    (c) Raoul Origa 2024
+    LuaUI Invisible Box
+    If you’d like to support me and my projects, I would greatly appreciate a donation: https://www.paypal.com/paypalme/kikkus
+]]
+
+function draw()
+   --drawRectangle(0, 0, size[1], size[2], {1,1,1,1})
+end
+
+function onMouseDown()
+    get(cl)
+    return true
+end
